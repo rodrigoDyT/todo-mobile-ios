2f424f7febb956b1e6a738525c338673b3893c10
rodrigoDyT <rod.dyt800@gmail.com>
2016-11-12 16:04:18 -0200

    include default look to all views(inspired by Heroku- Theyre awesome)

--- PowerTodo/PowerTodo/User.swift
+++ PowerTodo/PowerTodo/User.swift
@@ -4,12 +4,16 @@
 //
 //  Created by Rodrigo on 9/28/16.
 //  Copyright © 2016 SG Solutions. All rights reserved.
-//
 
 import Foundation
 import Alamofire
 
 class User {
+    
+    enum Methods {
+        case createUser
+        case getToken
+    }
     
     var name, email,password: String!
     init(name: String, email: String,password: String){
@@ -18,23 +22,37 @@ class User {
         self.password = password
     }
     
-    func signUp() {
+    func signUp(){
+        self.performRequest(method: Methods.createUser)
+    }
+    
+    func signIn(){
+        self.performRequest(method: Methods.getToken)
+    }
+    
+    func performRequest(method: Methods) {
         let parameters: Parameters = [
             "name" : self.name,
             "email" : self.email,
             "password" : self.password]
         
-        Alamofire.request(UserRouter.createUser(parameters: parameters)).responseJSON {
+        let urlRequest: UserRouter!
+        switch method {
+        case .createUser:
+            urlRequest = UserRouter.createUser(parameters: parameters)
+        case .getToken:
+            urlRequest = UserRouter.getUserToken(parameters: parameters)
+        }
+        
+        Alamofire.request(urlRequest).responseJSON {
             response in
-            
-            
             
             // First make sure you got back a dictionary, if that's what you expect
             guard let json = response.result.value as? [String : AnyObject] else {
                 print("Failed to get expected response from webserver.")
                 return
             }
-            
+            print(json)
             // Then make sure you get the actual key/value types you expect
             guard let userToken = json["token"] as? String else {
                 print("Failed to get data from webserver")
@@ -46,9 +64,6 @@ class User {
         
     }
     
-    func signIn(){
-        
-    }
     
     func setUserTokenDefault(token: String){
         let defaults = UserDefaults.standard
